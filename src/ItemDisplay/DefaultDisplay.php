894b7dd3c07837c8903defe09e40953484dfd77f
sugarfixx <ingar.torsrud@me.com>
2021-02-19 13:34:25 +0100

    Add default display

---
+++ src/ItemDisplay/DefaultDisplay.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace ItemDisplay;
+
+
+class DefaultDisplay implements ItemsInterface
+{
+    private $title;
+
+    public function __construct($title = "Default")
+    {
+        $this->title = $title;
+    }
+
+    public function makeDisplay() : Item
+    {
+        $display = new Item();
+        $display->setTitle($this->title);
+        $display->setImage('Default Image');
+        return $display;
+    }
+}
